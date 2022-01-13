b24d53f4422a90dce4a4e4fc2d9588b5d86dd3ad
itsar-t <rasti93@gmail.com>
2022-01-13 18:30:15 +0100

    script halloj

---
+++ Skippo/Assets/Scripts/Halloj.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Ett test script
+public class Halloj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
